ca30b16132854eb1f7131be38fb0209388bbffbe
Mattias <mattias@jurak.(none)>
2013-03-18 05:00:40 +0200

    Replace base64decode with a nicer equivalent, also take use of browser atob function if present.

--- static/idCard.js
+++ static/idCard.js
@@ -879,63 +879,47 @@ function bin2hex(bin) {
     return hex;
 }
 
-/* I have copied the routine for decoding BASE64 from
-   http://www.onicos.com/staff/iz/amuse/javascript/expert/base64.txt */
-
-var base64chr = new Array(-1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, 62, -1, -1, -1, 63,
-52, 53, 54, 55, 56, 57, 58, 59, 60, 61, -1, -1, -1, -1, -1, -1, -1, 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14,
-15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, -1, -1, -1, -1, -1, -1, 26, 27, 28, 29, 30, 31, 32, 33, 34, 35, 36, 37, 38, 39, 40,
-41, 42, 43, 44, 45, 46, 47, 48, 49, 50, 51, -1, -1, -1, -1, -1);
-
-function base64decode(str) {
-    var c1, c2, c3, c4;
-    var i, len, out;
-    len = str.length;
-    i = 0;
-    out = "";
-    while (i < len) {
-        /* c1 */
-        do {
-            c1 = base64chr[str.charCodeAt(i++) & 0xff];
-        } while (i < len && c1 == -1);
-        if (c1 == -1) {
-            break;
-        }
-
-        /* c2 */
-        do {
-            c2 = base64chr[str.charCodeAt(i++) & 0xff];
-        } while (i < len && c2 == -1);
-        if (c2 == -1) {
-            break;
-        }
-        out += String.fromCharCode((c1 << 2) | ((c2 & 0x30) >> 4));
-
-        /* c3 */
-        do {
-            c3 = str.charCodeAt(i++) & 0xff;
-            if (c3 == 61) {
-                return out;
-            }
-            c3 = base64chr[c3];
-        } while (i < len && c3 == -1);
-        if (c3 == -1) {
-            break;
-        }
-        out += String.fromCharCode(((c2 & 0XF) << 4) | ((c3 & 0x3C) >> 2));
-
-        /* c4 */
-        do {
-            c4 = str.charCodeAt(i++) & 0xff;
-            if (c4 == 61) {
-                return out;
-            }
-            c4 = base64chr[c4];
-        } while (i < len && c4 == -1);
-        if (c4 == -1) {
-            break;
-        }
-        out += String.fromCharCode(((c3 & 0x03) << 6) | c4);
+function base64decode(data) {
+    if (typeof atob == 'function') {
+        return atob(data);
     }
-    return out;
+
+    // The following code if borrowed from
+    // http://phpjs.org/functions/base64_decode/
+    // Thanks!
+    var b64 = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/=";
+    var o1, o2, o3, h1, h2, h3, h4, bits, i = 0,
+        ac = 0,
+        dec = "",
+        tmp_arr = [];
+
+    if (!data) {
+        return data;
+    }
+
+    data += '';
+
+    do { // unpack four hexets into three octets using index points in b64
+        h1 = b64.indexOf(data.charAt(i++));
+        h2 = b64.indexOf(data.charAt(i++));
+        h3 = b64.indexOf(data.charAt(i++));
+        h4 = b64.indexOf(data.charAt(i++));
+
+        bits = h1 << 18 | h2 << 12 | h3 << 6 | h4;
+
+        o1 = bits >> 16 & 0xff;
+        o2 = bits >> 8 & 0xff;
+        o3 = bits & 0xff;
+
+        if (h3 == 64) {
+            tmp_arr[ac++] = String.fromCharCode(o1);
+        } else if (h4 == 64) {
+            tmp_arr[ac++] = String.fromCharCode(o1, o2);
+        } else {
+            tmp_arr[ac++] = String.fromCharCode(o1, o2, o3);
+        }
+    } while (i < data.length);
+
+    dec = tmp_arr.join('');
+    return dec;
 }
